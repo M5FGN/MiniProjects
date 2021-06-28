3f598b9cdc1cd56fef507e2bd671a02205c687b5
M5FGN <33698169+M5FGN@users.noreply.github.com>
2021-06-28 14:45:22 +0100

    Refactoring Magic 8 Ball and adding Readme

---
+++ Magic_Eight_Ball/README.md
@@ -0,0 +1,21 @@
+# Magic 8 Ball
+
+## Description
+
+Digital recreation of the classic Magic 8 Ball. 
+
+## MVP
+
+* Create a Magic 8 Ball visual
+* Display random answers from the list of standard answers
+
+## Technologies Used
+
+* HTML
+* CSS
+* JavaScript
+
+## Screenshots
+
+![Magic 8 Ball](https://firebasestorage.googleapis.com/v0/b/images-4783e.appspot.com/o/magic8ball%2Fmagic8ball.jpg?alt=media&token=3f84d313-a0bc-46aa-b193-370c22c9ee5b)  
+![Magic 8 Ball](https://firebasestorage.googleapis.com/v0/b/images-4783e.appspot.com/o/magic8ball%2Fmagic8ballmessage.jpg?alt=media&token=09f21297-3c83-460f-aa93-81e86a7bfa09)
--- Magic_Eight_Ball/magic8.js
+++ Magic_Eight_Ball/magic8.js
@@ -1,82 +1,114 @@
 function func_shake() {
-    
+
     document.getElementById("white_ball").style.display = "none";
     document.getElementById("blue_triangle").style.display = "inline-block";
 
-    const answer = Math.ceil(Math.random()*20);
+    // New code using an array 
+    // Random number generated between 0 and 19 
+    const answer = Math.floor(Math.random() * 20);
 
-    switch (answer) {
-        case 1 :
-            document.getElementById("message").innerText = ("It is certain.");
+    // Array of possible messages.
+    const text = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes – definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful."
+    ];
 
-            break;
-        case 2 :
-            document.getElementById("message").innerText = ("It is decidedly so.");
-            break;
-        case 3 :
-            document.getElementById("message").innerText = ("Without a doubt.");
-            break;
-        case 4 :
-            document.getElementById("message").innerText = ("Yes – definitely.");
-            break;
-        case 5 :
-            document.getElementById("message").innerText = ("You may rely on it.");
-            break;
-        case 6 :
-            document.getElementById("message").innerText = ("As I see it, yes.");
-            break;
-        case 7 :
-            document.getElementById("message").innerText = ("Most likely.");
-            break;
-        case 8 :
-            document.getElementById("message").innerText = ("Outlook good.");
-            break;
-        case 9 :
-            document.getElementById("message").innerText = ("Yes.");
-            break;
-        case 10 :
-            document.getElementById("message").innerText = ("Signs point to yes.");
-            break;
-        case 11 :
-            document.getElementById("message").innerText = ("Reply hazy, try again.");
-            break;
-        case 12 :
-            document.getElementById("message").innerText = ("Ask again later.");
-            break;
-        case 13 :
-            document.getElementById("message").innerText = ("Better not tell you now.");
-            break;
-        case 14 :
-            document.getElementById("message").innerText = ("Cannot predict now.");
-            break;
-        case 15 :
-            document.getElementById("message").innerText = ("Concentrate and ask again.");
-            break;      
-        case 16 :
-            document.getElementById("message").innerText = ("Don't count on it.");
-            break;
-        case 17 :
-            document.getElementById("message").innerText = ("My reply is no.");
-            break;
-        case 18 :
-            document.getElementById("message").innerText = ("My sources say no.");
-            break;
-        case 19 :
-            document.getElementById("message").innerText = ("Outlook not so good.");
-            break;
-        case 20 :
-            document.getElementById("message").innerText = ("Very doubtful.");
-            break;  
-        default :
-            document.getElementById("message").innerText = ("Invalid");
-            break;          
-        }
-    }
+    //  Message generated using the number generated at "answer" as the index position of the text array
+    const message = text[answer];
+    document.getElementById("message").innerText = (message);
+
+    // Original code with switch case.
+
+    // const answer = Math.ceil(Math.random()*20);
+
+    // switch (answer) {
+    //     case 1 :
+    //         document.getElementById("message").innerText = ("It is certain.");
+    //         break;
+    //     case 2 :
+    //         document.getElementById("message").innerText = ("It is decidedly so.");
+    //         break;
+    //     case 3 :
+    //         document.getElementById("message").innerText = ("Without a doubt.");
+    //         break;
+    //     case 4 :
+    //         document.getElementById("message").innerText = ("Yes – definitely.");
+    //         break;
+    //     case 5 :
+    //         document.getElementById("message").innerText = ("You may rely on it.");
+    //         break;
+    //     case 6 :
+    //         document.getElementById("message").innerText = ("As I see it, yes.");
+    //         break;
+    //     case 7 :
+    //         document.getElementById("message").innerText = ("Most likely.");
+    //         break;
+    //     case 8 :
+    //         document.getElementById("message").innerText = ("Outlook good.");
+    //         break;
+    //     case 9 :
+    //         document.getElementById("message").innerText = ("Yes.");
+    //         break;
+    //     case 10 :
+    //         document.getElementById("message").innerText = ("Signs point to yes.");
+    //         break;
+    //     case 11 :
+    //         document.getElementById("message").innerText = ("Reply hazy, try again.");
+    //         break;
+    //     case 12 :
+    //         document.getElementById("message").innerText = ("Ask again later.");
+    //         break;
+    //     case 13 :
+    //         document.getElementById("message").innerText = ("Better not tell you now.");
+    //         break;
+    //     case 14 :
+    //         document.getElementById("message").innerText = ("Cannot predict now.");
+    //         break;
+    //     case 15 :
+    //         document.getElementById("message").innerText = ("Concentrate and ask again.");
+    //         break;      
+    //     case 16 :
+    //         document.getElementById("message").innerText = ("Don't count on it.");
+    //         break;
+    //     case 17 :
+    //         document.getElementById("message").innerText = ("My reply is no.");
+    //         break;
+    //     case 18 :
+    //         document.getElementById("message").innerText = ("My sources say no.");
+    //         break;
+    //     case 19 :
+    //         document.getElementById("message").innerText = ("Outlook not so good.");
+    //         break;
+    //     case 20 :
+    //         document.getElementById("message").innerText = ("Very doubtful.");
+    //         break;  
+    //     default :
+    //         document.getElementById("message").innerText = ("Invalid");
+    //         break;          
+    //     }
+}
 
 
 function func_reset() {
     document.getElementById("white_ball").style.display = "inline-block";
     document.getElementById("blue_triangle").style.display = "none";
-    document.getElementById("message"),innerText=" ";
+    document.getElementById("message").innerText = " ";
 }
-
