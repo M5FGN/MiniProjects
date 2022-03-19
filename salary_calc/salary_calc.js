cb7468b04c0a8c5a9c9f61ff710f2a4b111e004e
M5FGN <m5fgn@outlook.com>
2022-03-19 21:15:25 +0000

    Tidy Up to allow comparison to be added and easier year updates

--- salary_calc/salary_calc.js
+++ salary_calc/salary_calc.js
@@ -1,32 +1,35 @@
 function reset() {
-    document.getElementById("yearErr1").style.visibility = "hidden";
-    document.getElementById("salErr1").style.visibility = "hidden";
-    document.getElementById("daysErr1").style.visibility = "hidden";
-    document.getElementById("hoursErr1").style.visibility = "hidden";
+    document.getElementById("taxYearErr").style.visibility = "hidden";
+    document.getElementById("salaryErr").style.visibility = "hidden";
+    document.getElementById("daysErr").style.visibility = "hidden";
+    document.getElementById("hoursErr").style.visibility = "hidden";
 
+    document.getElementById("outSalary").textContent = " ";
+    document.getElementById("outTaxInc").textContent = " ";
+    document.getElementById("outTaxPaid").textContent = " ";
+    document.getElementById("outNIPaid").textContent = " ";
+    document.getElementById("outTakeHome").textContent = " ";
 }
-
 
 
 function inputCheck() {
 
-
     errors = true;
 
-    if (!document.querySelector('input[name="taxYr1"]:checked').value) {
-        document.getElementById("yearErr1").style.visibility = "visible";
+    if (!document.querySelector('input[name="taxYear"]:checked').value) {
+        document.getElementById("taxYearErr").style.visibility = "visible";
         errors = false;
     }
-    if (!document.getElementById("salVal1").value) {
-        document.getElementById("salErr1").style.visibility = "visible";
+    if (!document.getElementById("salaryVal").value) {
+        document.getElementById("salaryErr").style.visibility = "visible";
         errors = false;
     }
-    if (!document.getElementById("days1").value) {
-        document.getElementById("daysErr1").style.visibility = "visible";
+    if (!document.getElementById("days").value) {
+        document.getElementById("daysErr").style.visibility = "visible";
         errors = false;
     }
-    if (!document.getElementById("hours1").value) {
-        document.getElementById("hoursErr1").style.visibility = "visible";
+    if (!document.getElementById("hours").value) {
+        document.getElementById("hoursErr").style.visibility = "visible";
         errors = false;
     }
 
@@ -35,26 +38,26 @@ function inputCheck() {
 
 function calculateSalary() {
 
-    let salary1 = document.getElementById("salVal1").value;
-    let hours1 = document.getElementById("hours1").value;
-    let days1 = document.getElementById("days1").value;
+    let salary = document.getElementById("salaryVal").value;
+    let hours = document.getElementById("hours").value;
+    let days = document.getElementById("days").value;
 
-    let unit1 = document.querySelector('#salUnit1').value;
+    let unit = document.querySelector('#salUnit').value;
 
-    if (unit1 == "year") {
-        return salary1;
+    if (unit == "year") {
+        return salary ;
 
-    } else if (unit1 == "month") {
-        return salary1 * 12;
+    } else if (unit == "month") {
+        return salary  * 12;
 
-    } else if (unit1 == "week") {
-        return salary1 * 52;
+    } else if (unit == "week") {
+        return salary  * 52;
 
-    } else if (unit1 == "day") {
-        return salary1 * days1 * 52;
+    } else if (unit == "day") {
+        return salary  * days * 52;
 
-    } else if (unit1 == "hour") {
-        return salary1 * hours1 * 52;
+    } else if (unit == "hour") {
+        return salary  * hours * 52;
     }
 }
 
@@ -62,9 +65,10 @@ function calculateTaxInc() {
 
     taxInc = 0;
 
-    let year1 = document.querySelector('input[name="taxYr1"]:checked').value;
+    let year = document.querySelector('input[name="taxYear"]:checked').value;
 
     // Starting Points and Percentage paid for each tax bracket
+// Source: https://www.gov.uk/scottish-income-tax
 
     //taxYearOptA currently = 2021
     const taxYearOptA = {
@@ -75,13 +79,17 @@ function calculateTaxInc() {
         starter: 12571
     };
 
-    if (year1 == "2021") {
-        if (salary1 > taxYearOptA.starter) {
-            taxInc = salary1 - taxYearOptA.starter;
+    if (year == "optA") {
+        if (salary 
+ > taxYearOptA.starter) {
+            taxInc = salary 
+ - taxYearOptA.starter;
         }
-    } else if (year1 == "2022") {
-        if (salary1 > taxYearOptA.starter) {
-            taxInc = salary1 - taxYearOptA.starter;
+    } else if (year == "optB") {
+        if (salary 
+ > taxYearOptA.starter) {
+            taxInc = salary 
+ - taxYearOptA.starter;
         }
     }
 
@@ -92,9 +100,10 @@ function calculateTaxPaid() {
 
     let taxPaid = 0;
 
-    let year1 = document.querySelector('input[name="taxYr1"]:checked').value;
+    let year = document.querySelector('input[name="taxYear"]:checked').value;
 
     // Starting Points and Percentage paid for each tax bracket
+    // Source: https://www.gov.uk/scottish-income-tax
 
     //taxYearOptA currently = 2021
     const taxYearOptA = {
@@ -123,32 +132,42 @@ function calculateTaxPaid() {
         topPercent: 0.46
     };
 
-    if (year1 == "2021") {
+    if (year == "optA") {
         console.log("2021");
-        if (salary1 > taxYearOptA.top) {
-            taxPaid += (salary1 - taxYearOptA.top) * taxYearOptA.topPercent;
+        if (salary 
+ > taxYearOptA.top) {
+            taxPaid += (salary 
+     - taxYearOptA.top) * taxYearOptA.topPercent;
             taxPaid += (taxYearOptA.top - taxYearOptA.higher) * taxYearOptA.higherPercent;
             taxPaid += (taxYearOptA.higher - taxYearOptA.intermediate) * taxYearOptA.intermediatePercent;
             taxPaid += (taxYearOptA.intermediate - taxYearOptA.basic) * taxYearOptA.basicPercent;
             taxPaid += (taxYearOptA.basic - taxYearOptA.starter) * taxYearOptA.starterPercent;
             console.log("top");
-        } else if (salary1 > taxYearOptA.higher) {
-            taxPaid += (salary1 - taxYearOptA.higher) * taxYearOptA.higherPercent;
+        } else if (salary 
+ > taxYearOptA.higher) {
+            taxPaid += (salary 
+     - taxYearOptA.higher) * taxYearOptA.higherPercent;
             taxPaid += (taxYearOptA.higher - taxYearOptA.intermediate) * taxYearOptA.intermediatePercent;
             taxPaid += (taxYearOptA.intermediate - taxYearOptA.basic) * taxYearOptA.basicPercent;
             taxPaid += (taxYearOptA.basic - taxYearOptA.starter) * taxYearOptA.starterPercent;
             console.log("higher");
-        } else if (salary1 > taxYearOptA.intermediate) {
-            taxPaid += (salary1 - taxYearOptA.intermediate) * taxYearOptA.intermediatePercent;
+        } else if (salary 
+ > taxYearOptA.intermediate) {
+            taxPaid += (salary 
+     - taxYearOptA.intermediate) * taxYearOptA.intermediatePercent;
             taxPaid += (taxYearOptA.intermediate - taxYearOptA.basic) * taxYearOptA.basicPercent;
             taxPaid += (taxYearOptA.basic - taxYearOptA.starter) * taxYearOptA.starterPercent;
             console.log("intermediate");
-        } else if (salary1 > taxYearOptA.basic) {
-            taxPaid += (salary1 - taxYearOptA.basic) * taxYearOptA.basicPercent;
+        } else if (salary 
+ > taxYearOptA.basic) {
+            taxPaid += (salary 
+     - taxYearOptA.basic) * taxYearOptA.basicPercent;
             taxPaid += (taxYearOptA.basic - taxYearOptA.starter) * taxYearOptA.starterPercent;
             console.log("basic");
-        } else if (salary1 > taxYearOptA.starter) {
-            taxPaid += (salary1 - taxYearOptA.starter) * taxYearOptA.starterPercent;
+        } else if (salary 
+ > taxYearOptA.starter) {
+            taxPaid += (salary 
+     - taxYearOptA.starter) * taxYearOptA.starterPercent;
             console.log("starter");
         } else {
             taxPaid = 0;
@@ -156,32 +175,42 @@ function calculateTaxPaid() {
         }
 
 
-    } else if (year1 == "2022") {
+    } else if (year == "optB") {
         console.log("2022");
-        if (salary1 > taxYearOptB.top) {
-            taxPaid += (salary1 - taxYearOptB.top) * taxYearOptB.topPercent;
+        if (salary 
+ > taxYearOptB.top) {
+            taxPaid += (salary 
+     - taxYearOptB.top) * taxYearOptB.topPercent;
             taxPaid += (taxYearOptB.top - taxYearOptB.higher) * taxYearOptB.higherPercent;
             taxPaid += (taxYearOptB.higher - taxYearOptB.intermediate) * taxYearOptB.intermediatePercent;
             taxPaid += (taxYearOptB.intermediate - taxYearOptB.basic) * taxYearOptB.basicPercent;
             taxPaid += (taxYearOptB.basic - taxYearOptB.starter) * taxYearOptB.starterPercent;
             console.log("top");
-        } else if (salary1 > taxYearOptB.higher) {
-            taxPaid += (salary1 - taxYearOptB.higher) * taxYearOptB.higherPercent;
+        } else if (salary 
+ > taxYearOptB.higher) {
+            taxPaid += (salary 
+     - taxYearOptB.higher) * taxYearOptB.higherPercent;
             taxPaid += (taxYearOptB.higher - taxYearOptB.intermediate) * taxYearOptB.intermediatePercent;
             taxPaid += (taxYearOptB.intermediate - taxYearOptB.basic) * taxYearOptB.basicPercent;
             taxPaid += (taxYearOptB.basic - taxYearOptB.starter) * taxYearOptB.starterPercent;
             console.log("higher");
-        } else if (salary1 > taxYearOptB.intermediate) {
-            taxPaid += (salary1 - taxYearOptB.intermediate) * taxYearOptB.intermediatePercent;
+        } else if (salary 
+ > taxYearOptB.intermediate) {
+            taxPaid += (salary 
+     - taxYearOptB.intermediate) * taxYearOptB.intermediatePercent;
             taxPaid += (taxYearOptB.intermediate - taxYearOptB.basic) * taxYearOptB.basicPercent;
             taxPaid += (taxYearOptB.basic - taxYearOptB.starter) * taxYearOptB.starterPercent;
             console.log("intermediate");
-        } else if (salary1 > taxYearOptB.basic) {
-            taxPaid += (salary1 - taxYearOptB.basic) * taxYearOptB.basicPercent;
+        } else if (salary 
+ > taxYearOptB.basic) {
+            taxPaid += (salary 
+     - taxYearOptB.basic) * taxYearOptB.basicPercent;
             taxPaid += (taxYearOptB.basic - taxYearOptB.starter) * taxYearOptB.starterPercent;
             console.log("basic");
-        } else if (salary1 > taxYearOptB.starter) {
-            taxPaid += (salary1 - taxYearOptB.starter) * taxYearOptB.starterPercent;
+        } else if (salary 
+ > taxYearOptB.starter) {
+            taxPaid += (salary 
+     - taxYearOptB.starter) * taxYearOptB.starterPercent;
             console.log("starter");
         } else {
             taxPaid = 0;
@@ -197,19 +226,21 @@ function calculateNI() {
 
     let NIPaidWeekly = 0;
 
-    let year1 = document.querySelector('input[name="taxYr1"]:checked').value;
+    let year = document.querySelector('input[name="taxYear"]:checked').value;
     let weeklySalary = calculateSalary()/52;
     console.log(weeklySalary);
 
     // Starting Points and Percentage paid for each NI band
-    //NIYearA currently = 2021
+    // Source: https://www.moneyhelper.org.uk/en/work/employment/how-does-national-insurance-work-and-should-you-be-paying-it
+
+    // NIYearOptionA currently = 2021
     const NIYearOptionA = {
         band1: 184,
         band1Percent: 0.12,
         band2: 967,
         band2Percent: 0.02
     };
-    //NIYearB currently = 2022
+    // NIYearOptB currently = 2022
     const NIYearOptB = {
         band1: 184,
         band1Percent: 0.12,
@@ -217,14 +248,14 @@ function calculateNI() {
         band2Percent: 0.02
     };
 
-    if (year1 = "2021"){
+    if (year = "optA"){
         if (weeklySalary > NIYearOptionA.band2) {
             NIPaidWeekly += (weeklySalary-NIYearOptionA.band2)*NIYearOptionA.band2Percent;
             NIPaidWeekly += (NIYearOptionA.band2-NIYearOptionA.band1)*NIYearOptionA.band1Percent;
         } else if (weeklySalary>NIYearOptionA.band1){
             NIPaid += (weeklySalary-NIYearOptionA.band1)*NIYearOptionA.band1Percent;
         }
-    } else if (year1 = "2022"){
+    } else if (year = "optB"){
         if (weeklySalary > NIYearOptB.band2) {
             NIPaidWeekly += (weeklySalary-NIYearOptB.band2)*NIYearOptB.band2Percent;
             NIPaidWeekly += (NIYearOptB.band2-NIYearOptB.band1)*NIYearOptB.band1Percent;
@@ -239,29 +270,30 @@ function calculateNI() {
 
 }
 
-function calculate1() {
+function calculate() {
 
     reset();
 
     if (inputCheck() == true) {
         console.log("no errors");
 
-        salary1 = calculateSalary();
-        console.log(salary1);
+        salary  = calculateSalary();
+        console.log(salary 
+);
         taxInc = calculateTaxInc();
         taxPaid = calculateTaxPaid();
         console.log(taxPaid)
         NIPaid = calculateNI();
-        takeHome = salary1 - taxPaid - NIPaid;
+        takeHome = salary  - taxPaid - NIPaid;
 
 
 
-        // FIXME - salary1 to two decimal places.
-        document.getElementById("outSal1").textContent = salary1;
-        document.getElementById("outTaxInc1").textContent = taxInc.toFixed(2);
-        document.getElementById("outTax1").textContent = taxPaid.toFixed(2);
-        document.getElementById("outNI1").textContent = NIPaid.toFixed(2);
-        document.getElementById("outTakeHome1").textContent = takeHome.toFixed(2);
+        // FIXME - salary  to two decimal places.
+        document.getElementById("outSalary").textContent = salary ;
+        document.getElementById("outTaxInc").textContent = taxInc.toFixed(2);
+        document.getElementById("outTaxPaid").textContent = taxPaid.toFixed(2);
+        document.getElementById("outNIPaid").textContent = NIPaid.toFixed(2);
+        document.getElementById("outTakeHome").textContent = takeHome.toFixed(2);
 
     } else {
         console.log("errors in input");
