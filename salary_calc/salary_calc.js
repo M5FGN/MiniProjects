d74928c8bfce267038af33d24975ca0161b17e6a
M5FGN <m5fgn@outlook.com>
2022-03-19 15:54:59 +0000

    Add error tracking

---
+++ salary_calc/salary_calc.js
@@ -0,0 +1,41 @@
+function inputCheck() {
+
+    document.getElementById("yearErr1").style.visibility = "hidden";
+    document.getElementById("salErr1").style.visibility = "hidden";
+    document.getElementById("daysErr1").style.visibility = "hidden";
+    document.getElementById("hoursErr1").style.visibility = "hidden";
+
+    errors = true;
+
+    year1 = document.getElementById("taxYr1").value;
+    salary1 = document.getElementById("salVal1").value;
+    hours1 = document.getElementById("hours1").value;
+    days1 = document.getElementById("days1").value;
+
+    if (!year1) {
+        document.getElementById("yearErr1").style.visibility = "visible";
+        errors = false;
+    }
+    if (!salary1) {
+        document.getElementById("salErr1").style.visibility = "visible";
+        errors = false;
+    }
+    if (!days1) {
+        document.getElementById("daysErr1").style.visibility = "visible";
+        errors = false;
+    }
+    if (!hours1) {
+        document.getElementById("hoursErr1").style.visibility = "visible";
+        errors = false;
+    }
+
+    return errors;
+}
+
+function calculateSal1() {
+    if (inputCheck() == false) {
+        console.log("errors in input");
+    } else {
+        console.log("no errors");
+    };
+}
