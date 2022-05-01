cf8615a01462b7314abddf7b02f0414f2336f5eb
m5fgn <m5fgn@outlook.com>
2022-05-01 10:16:14 +0100

    Adding Local Storage to reopen previously opened doors

--- Advent_Calendar/advent.js
+++ Advent_Calendar/advent.js
@@ -1,12 +1,25 @@
-function open_door () 
-    {
-    document.getElementById("door_closed_"+door).style.display = "none";
-    document.getElementById("door_open_"+door).style.display = "block";
+function onLoad() {
+    for (let door = 1; door < 25; door++) {
+        if (localStorage.getItem("door " + door) == "open") {
+            document.getElementById("door_closed_" + door).style.display = "none";
+            document.getElementById("door_open_" + door).style.display = "block";
+        };
     }
+}
 
-    // Door number 1 which was added to the function call is used in the function.
-function reset_door () 
-    {
-    document.getElementById("door_closed_"+door).style.display = "block";
-    document.getElementById("door_open_"+door).style.display = "none";
-    }
+function open_door() {
+    document.getElementById("door_closed_" + door).style.display = "none";
+    document.getElementById("door_open_" + door).style.display = "block";
+
+    let doorNum = `door ${door}`;
+    localStorage.setItem(doorNum, "open");
+
+}
+
+function reset_door() {
+    document.getElementById("door_closed_" + door).style.display = "block";
+    document.getElementById("door_open_" + door).style.display = "none";
+
+    let doorNum = `door ${door}`;
+    localStorage.removeItem(doorNum);
+}
